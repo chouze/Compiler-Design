8c58cc160682c5ebc3f3318c015bc2852e64325f
Deacide <carlind0@students.rowan.edu>
2016-02-28 20:02:35 -0500

    Updates Made to Statement()

---
+++ Compiler Test 1/src/Driver.java
@@ -0,0 +1,15 @@
+import parsing.Parser;
+
+
+public class Driver {
+
+	public static void main(String[] args) 
+	{
+		Parser parser = new Parser();// TODO Auto-generated method stub
+
+	}
+
+}
+
+
+
---
+++ Compiler Test 1/src/parsing/ParsingException.java
@@ -0,0 +1,26 @@
+package parsing;
+import lexing.node.Token;
+
+public class ParsingException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public ParsingException(String message){
+		super(message);
+	}
+
+	public ParsingException(){
+		super();
+	}
+	
+	public ParsingException(Token token){
+		super("error on token: " +  token.getClass().getName() + ": " + token.toString() + " at line: " + token.getLine() + " position: " + token.getPos());
+	}
+	
+	public ParsingException(Token token, String wantedName){
+		super("error on token: " +  token.getClass().getName() + ": " + token.toString() + " at line: " + token.getLine() + " position: " + token.getPos()
+		+ "\nwanted to eat: " + wantedName);
+	}
+}
