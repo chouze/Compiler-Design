7f5424801db3e2772e34b8ffc64afbcff04f5fea
cblack7057 <cblack7057@gmail.com>
2016-03-19 18:06:53 -0400

    Update ParserDriver.java

--- LR Parser/src/ParserDriver.java
+++ LR Parser/src/ParserDriver.java
@@ -8,20 +8,9 @@ public class ParserDriver {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
-		//Parser parser = new Parser();
-		ArrayList<TableState> states = new ArrayList<TableState>();
+		Parser parser = new Parser();
 		
-		Scanner scanner = new Scanner(new File("Grammar3_1_Output.csv"));
-		String header = scanner.nextLine();
-		System.out.println(header);
-		while (scanner.hasNextLine()) {
-			//System.out.println(scanner.nextLine());
-			states.add(new TableState(header, scanner.nextLine()));
-		}
-		scanner.close();
-		//for(TableState state: states){
-		//	System.out.println(state);
-		//}
 	}
 
 }
+
