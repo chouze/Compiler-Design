db46967a0691571cdcd3d0edbee4e987ac0d372e
Deacide <carlind0@students.rowan.edu>
2016-03-23 10:08:54 -0400

    Sablecc Program for Eclipse

---
+++ SableCC Program/src/lexing/Translation.java
@@ -0,0 +1,14 @@
+package lexing;
+
+/** overide certain methods from DepthFirstAdapter 
+ *  to implement a translation as the input is parsed
+ */
+
+ import lexing.analysis.*;
+ import lexing.node.*;
+ import java.util.*;
+
+public class Translation extends DepthFirstAdapter
+{
+
+} 
